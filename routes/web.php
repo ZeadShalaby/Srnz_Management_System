66cfdead72f02490c76e7f61755d9a43f27a6e9a
Zead_Shalaby <you@example.com>
2023-07-05 21:50:32 +0300

    Database

--- routes/web.php
+++ routes/web.php
@@ -20,46 +20,6 @@ use App\Http\Controllers\InterestedsController;
 */
 
 Route::get('/', function () {
-    return redirect('/logout');
+    return redirect('/welcome');
 });
 
-Route::get('/login', [UsersController::class, 'loginIndex']);
-Route::post('/login/checklogin', [UsersController::class, 'checklogin']);
-Route::get('/login/checklogin',function (){redirect(route('logout'));});
-Route::get('/logout', [UsersController::class, 'logout']);
-
-
-
-//Route::middleware(CheckAdminRole::class)->group(function () {
-    // controller with rescource
-    Route::resource('/orders', OrdersController::class);
-    Route::resource('/departments', DepartmentsController::class);
-    Route::resource('/users', UsersController::class);
-    Route::resource('/interesteds', InterestedsController::class);
-   // });
-    //restore
-    Route::get('/departments-restore', [DepartmentsController::class, 'restore_index'])->name('departments.restore.index');
-    Route::get('/departments/restore/do', [DepartmentsController::class, 'restore'])->name('departments.restore');
-    
-    //favourite 
-    Route::POST('/orders-favoruite', [OrdersController::class, 'favoruite'])->name('orders.favourite');
-    
-    //autocompleteSearch
-    Route::get('/autocomplete-search-departments', [DepartmentsController::class, 'autocompleteSearch']);
-    Route::get('/autocomplete-search-orders', [OrdersController::class, 'autocompleteSearch']);
-    
-    //search
-    Route::POST('/search-orders', [OrdersController::class, 'search_orders'])->name('orders.search');
-    Route::POST('/search-departments', [DepartmentsController::class, 'search_departments'])->name('departments.search');
-    
-
-
-
-/*Route::middleware(CheckAdminRole::class)->group(function () {
-    //admin route
-Route::resource('/posts', postsController::class);
-
-Route::get('posts/restore/one/{id}', [postsController::class, 'restore'])->name('post.restore');
-
-Route::get('posts/restore_all', [postsController::class, 'restore_all'])->name('post.restore_all');
-});*/
